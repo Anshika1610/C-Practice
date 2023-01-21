eaa6678631390adfd61ec199e43cd14c282bf7cc
Anshika1610 <71184320+Anshika1610@users.noreply.github.com>
2023-01-21 18:25:19 +0530

    Create ObjectOverriding.cpp

---
+++ ObjectOverriding.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Base{
+  public:
+    void display(){
+        cout<<"Display of Parent."<<endl;
+    }
+};
+
+class Derived:public Base{
+  public:
+    void display(){ 
+        //The name of this function is the same as the name of the function in parent class
+        cout<<"Display of Child."<<endl;
+    }
+};
+
+int main(){
+    Derived d;
+    d.display(); //This will call it's own function 
+}
