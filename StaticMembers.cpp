0e7fe62a6c5bc1eb0c4036a8e0b065c7c05bad69
Anshika1610 <71184320+Anshika1610@users.noreply.github.com>
2023-01-27 22:27:55 +0530

    Update StaticMembers.cpp

--- StaticMembers.cpp
+++ StaticMembers.cpp
@@ -12,6 +12,12 @@ class Test{
         a=10;
         count++;
     }
+    
+    static int getcount(){
+        //Note that only static members can be used in static methods
+        //'return a++;' would be an error
+        return count;
+    }
 };
 
 int Test::count=0;
@@ -23,4 +29,6 @@ int main()
     cout<<t2.count<<endl;
     t1.count=25;
     cout<<t2.count<<endl;  //Result is 25
+    cout<<Test::getcount()<<endl;  //Static member or function can be called by object or class
+    cout<<t1.getcount()<<endl;
 }
